7e716fe3da9f7779d0dd6afabe15844088c6fac9
hashmagic <hashmagic420@gmail.com>
2024-10-01 15:19:59 -0400

    Update server.js
    2nd

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const fastify = require('fastify')({ logger: true });
-const path = require('path');
-const fs = require('fs');
 const WebSocket = require('ws');
 
-// Serve static HTML
+// Serve the frontend
+const path = require('path');
+const fs = require('fs');
+
 fastify.get('/', (request, reply) => {
   const filePath = path.join(__dirname, 'index.html');
   const fileStream = fs.createReadStream(filePath);
   reply.type('text/html').send(fileStream);
 });
 
-// Start the Fastify server
-fastify.listen({ port: 3001 }, (err, address) => {
+// Start Fastify on port 3000 (Heroku will manage the port in production)
+fastify.listen(process.env.PORT || 3000, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
@@ -23,9 +24,9 @@ fastify.listen({ port: 3001 }, (err, address) => {
 
   wss.on('connection', (ws) => {
     ws.on('message', (message) => {
-      // Broadcast the message to all clients
+      // Broadcast drawing data to all connected clients
       wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
           client.send(message);
         }
       });
